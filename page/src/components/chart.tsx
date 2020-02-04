d204c95408707dd8a8c63283bd9f665b4ea34ccb
xubaoshi <948869516@qq.com>
2020-02-04 12:18:06 +0800

    add chart code

--- page/src/components/chart.tsx
+++ page/src/components/chart.tsx
@@ -1,24 +1,43 @@
 import * as React from 'react'
 import { getTableData } from '../utils/db'
-
+// import moment from 'moment'
+import echarts from 'echarts'
+import '../styles/chart.scss'
 export default class Chart extends React.Component<any, any> {
   constructor(props) {
     super(props)
     this.state = {
-      list: []
+      list: [],
+      dayChart: null,
+      weekChart: null,
+      monthChart: null,
+      option: {
+        tooltip: {
+          show: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 40, 10, 10, 20]
+          }
+        ]
+      }
     }
   }
   async componentDidMount() {
-    let arr = await getTableData()
-    arr = arr.map((item, index) => {
-      return {
-        ...item,
-        key: index
-      }
-    })
-    this.setState({
-      list: arr
-    })
+    this.update()
   }
   async update() {
     let arr = await getTableData()
@@ -30,13 +49,58 @@ export default class Chart extends React.Component<any, any> {
     })
     this.setState({
       list: arr
+    },() => {
+      this.generateCurrentDayData()
+      this.generateLastWeekData()
+      this.generateLastMonthData()
+    })
+  }
+  generateCurrentDayData() {
+    const { option } = this.state
+    const dayChart = echarts.init(document.getElementById('dayChart'))
+    dayChart.setOption(option)
+    this.setState({
+      dayChart
+    })
+  }
+  generateLastWeekData() {
+    const { option } = this.state
+    const weekChart = echarts.init(document.getElementById('weekChart'))
+    weekChart.setOption(option)
+    this.setState({
+      weekChart
+    })
+  }
+  generateLastMonthData() {
+    const { option } = this.state
+    const monthChart = echarts.init(document.getElementById('monthChart'))
+    monthChart.setOption(option)
+    this.setState({
+      monthChart
     })
   }
   render() {
     const { list } = this.state
     return (
       <div className="chart-wrap">
-        <div>断链统计图表</div>
+        <div className="item">
+          <div className="tit">本日统计</div>
+          <div className="con">
+            <div id="dayChart" className="chart"></div>
+          </div>
+        </div>
+        <div className="item">
+          <div className="tit">最近一周统计</div>
+          <div className="con">
+            <div id="weekChart" className="chart"></div>
+          </div>
+        </div>
+        <div className="item">
+          <div className="tit">最近一月统计</div>
+          <div className="con">
+            <div id="monthChart" className="chart"></div>
+          </div>
+        </div>
       </div>
     )
   }
--- readme.md
+++ readme.md
@@ -10,6 +10,9 @@ page    断链统计端页面
 ## 命令
 
 ```
+
+切换到 page server 目录下载对应依赖包
+
 npm run dev   ||  yarn run dev       开发环境
 npm run sever ||  yarn run server    开发环境 websocket 服务
 npm run build ||  yarn run build     生产环境
